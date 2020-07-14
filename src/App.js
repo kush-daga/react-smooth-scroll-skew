daf71eaef85fab7cb173b63fcc2a8711ee23370d
Kush Daga <kushdaga@hotmail.com>
2020-07-14 13:10:35 +0530

    completed v1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React, { useRef, useEffect } from "react";
+import useWindowSize from "./hooks/useWindowSize";
 import "./App.scss";
 
 import images from "./images/images";
 
 function App() {
+  //HOOK
+  const size = useWindowSize();
   //REF
   const app = useRef();
   const scrollContainer = useRef();
+
+  const skewConfigs = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0,
+  };
+
+  useEffect(() => {
+    document.body.style.height = `${
+      scrollContainer.current.getBoundingClientRect().height
+    }px`;
+  }, [size.height]);
+
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  }, []);
+
+  const skewScrolling = () => {
+    skewConfigs.current = window.scrollY;
+    skewConfigs.previous +=
+      (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+
+    //Variables
+
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const accelaration = difference / size.width;
+    const velocity = +accelaration;
+    const skew = velocity * 9.5;
+
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`;
+    requestAnimationFrame(() => skewScrolling());
+  };
 
   return (
     <div ref={app} className="App">
@@ -17,7 +54,7 @@ function App() {
               <img src={image} alt={`people ${index}`} />
             </div>
             <h2>
-              Skew <span className="outline">scroll</span>
+              Smooth Skew <span className="outline">scroll</span>
             </h2>
           </>
         ))}
